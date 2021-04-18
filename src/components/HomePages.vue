8226cef4988f73d2e5ff75bbbeba341df75d26b7
Victor654 <61770874+Victor654@users.noreply.github.com>
2021-04-18 09:49:52 -0500

    Update HomePages
    This view was modified by consuming the api and bringing the data from this

--- src/components/HomePages.vue
+++ src/components/HomePages.vue
@@ -1,30 +1,82 @@
 <template>
   <div class="hello">
-    <div class="container">
-      <div class="neon_card">
-        <h1>Hello</h1>
+    <div v-if="createDom" class="container">
+      <div v-for="(person, index) of people" v-bind:key="person.id"  class="neon_card">
+          <!-- <img src="../assets/fondo.jpg" alt=""> -->
+          <h1 class="title">{{ person.name }}</h1>
+          <p>{{homeWorlds[index].name}}</p>
       </div>
-      <div class="neon_card second">
-        <h1>Every one</h1>
+      <div class="pagination">
+        <b-pagination-nav pills size="sm" number-of-pages="10" base-url="#"></b-pagination-nav>
       </div>
-      <div class="neon_card third">
-        <h1>welcome</h1>
-      </div>
+    </div>
+    <div class="img" v-else>
+      <img src="../assets/spiner.gif" alt="">
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: 'HelloWorld',
+  data: function() {
+    return {
+      people: [],
+      homeWorlds: [],
+      pages: 1,
+      createDom: false
+    }
+  },
+  async created() {
+    for(let i=1; i<=10; i++){
+      if(i!=17){
+        await this.getPeople(i);
+        await this.getHomeworld(i);
+      }
+    }
+    this.createDom = true
+  },
+  methods: {
+   async getPeople(i)  {
+      try{
+        let res = await axios.get(process.env.VUE_APP_BACK_ROUTE + `people/${i}`)
+        this.people.push(res.data);
+      }catch(err){
+        console.log(err);
+      }
+    },
+    async getHomeworld(i) {
+      try{
+        let res = await axios.get(process.env.VUE_APP_BACK_ROUTE + `planets/${i}`)
+        this.homeWorlds.push(res.data);
+      }catch(err){
+        console.log(err);
+      }
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
   @mixin displayCenter{
-    display: flex;
+/*     display: block;
+/*     justify-content: center;
+    align-items: center; */ 
+  }
+
+  .img{
     justify-content: center;
     align-items: center;
+    display: flex;
+    margin-left: 5%;
+    margin-top: 22%;
+  }
+
+  .title{
+    text-align: center;
+    margin-block: 12%;
   }
 
   .hello{
@@ -32,7 +84,12 @@ export default {
     min-height: 100vh;
     background: #0000;
     color: #ffff;
-    @include displayCenter();
+    // @include displayCenter();
+  }
+
+  .pagination{
+    margin-top: 134%;
+    margin-left: 25%;
   }
   
 .container {
@@ -44,10 +101,11 @@ export default {
         .neon_card {
             width: 300px;
             height: 400px;
-            background: #000000;
-            margin: auto;
+            background-image: url("../assets/fondo.jpg");
+            margin: 10px;
+            left: 30px;
             position: relative;
-            @include displayCenter();
+            // @include displayCenter();
 
             &::before {
                 position: absolute;
@@ -71,32 +129,6 @@ export default {
                 z-index: -2;
                 filter: blur(50px);
             }
-        }
-
-        .second {
-            &::before {
-                background: linear-gradient(235deg, #ffbb00, #060c21, #d40063);
-
-            }
-
-            &::after {
-                background: linear-gradient(235deg, #ffbb00, #060c21, #d40063);
-
-            }
-
-        }
-
-        .third {
-            &::before {
-                background: linear-gradient(235deg, #e100ff, #060c21, #3700ff);
-
-            }
-
-            &::after {
-                background: linear-gradient(235deg, #e100ff, #060c21, #3700ff);
-
-            }
-
         }
     }
 </style>
