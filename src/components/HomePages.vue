6fc3629ddf1f6f03d704cd167d42cd4c6091f927
Victor654 <61770874+Victor654@users.noreply.github.com>
2021-04-19 09:34:38 -0500

    Update HomePages
    The pager was added, the responsive design and the router link was added

--- src/components/HomePages.vue
+++ src/components/HomePages.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="hello">
     <div v-if="createDom" class="container">
-      <div v-for="(person, index) of people" v-bind:key="person.id"  class="neon_card">
-          <!-- <img src="../assets/fondo.jpg" alt=""> -->
-          <h1 class="title">{{ person.name }}</h1>
-          <p>{{homeWorlds[index].name}}</p>
+      <div v-for="(person, index) of people" v-bind:key="index" @click="redirect(person)" class="neon_card">
+        <h1 class="title">{{ person.name }}</h1>
+        <p class="planets">PLANET: {{homeWorlds[index].name}}</p>
       </div>
-      <div class="pagination">
-        <b-pagination-nav pills  number-of-pages="10" base-url="#"></b-pagination-nav>
-      </div>
+      <b-button-group class="buttons">
+        <b-button @click="paginate(1, 10)" variant="outline-primary">1</b-button>
+        <b-button @click="paginate(11, 21)" variant="outline-primary">2</b-button>
+        <b-button @click="paginate(22, 31)" variant="outline-primary">3</b-button>
+      </b-button-group>
     </div>
-    <div class="img" v-else>
-      <img src="../assets/spiner.gif" alt="">
-    </div>
+  <div class="img" v-else>
+    <img src="../assets/spiner.gif" alt="">
+  </div>
   </div>
 </template>
 
@@ -20,12 +21,11 @@
 import axios from "axios";
 
 export default {
-  name: 'HelloWorld',
+  name: 'HomePages',
   data: function() {
     return {
       people: [],
       homeWorlds: [],
-      pages: 1,
       createDom: false
     }
   },
@@ -39,7 +39,7 @@ export default {
     this.createDom = true
   },
   methods: {
-   async getPeople(i)  {
+    async getPeople(i)  {
       try{
         let res = await axios.get(process.env.VUE_APP_BACK_ROUTE + `people/${i}`)
         this.people.push(res.data);
@@ -54,6 +54,23 @@ export default {
       }catch(err){
         console.log(err);
       }
+    },
+    async paginate(start, end){
+      this.createDom = false
+      this.people = []
+      this.homeWorlds = []
+      for(start; start<=end; start++){
+        if(start!=17){
+          await this.getPeople(start);
+          await this.getHomeworld(start);
+        }
+      }
+      this.createDom = true
+    },
+    redirect(person){
+      let urlArray = person.url.split("/")
+      let id = urlArray[urlArray.length-2]
+      this.$router.push({name:'Character', params:{id:id}})
     }
   }
 }
@@ -61,9 +78,14 @@ export default {
 
 <style lang="scss" scoped>
   @mixin displayCenter{
-/*     display: block;
-/*     justify-content: center;
+  /*  display: block; */
+  /*     justify-content: center;
     align-items: center; */ 
+  }
+
+  .buttons{ 
+    margin-left: 52%;
+    margin-top: 110%;
   }
 
   .img{
@@ -71,66 +93,160 @@ export default {
     align-items: center;
     display: flex;
     margin-left: 5%;
-    margin-top: 22%;
+    margin-top: 15%;
+  }
+
+  .planets{
+    text-align: center;
+    font-size: 24px;
+    color: #00daff;
   }
 
   .title{
     text-align: center;
     margin-block: 12%;
-  }
-
-  .hello{
-    width: 100%;
-    min-height: 100vh;
-    background: #0000;
+    font-size: 34px;
     color: #ffff;
-    // @include displayCenter();
   }
 
-  .pagination{
-    margin-top: 131%;
-    margin-left: 25%;
+  .button{
+    margin-left: 49%;
+    margin-top: 109%;
   }
-  
-.container {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
-        width: 100%;
-        height: 100%;
 
-        .neon_card {
-            width: 300px;
-            height: 400px;
-            background-image: url("../assets/fondo.jpg");
-            margin: 10px;
-            left: 30px;
-            position: relative;
-            // @include displayCenter();
+  .container {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    width: 100%;
+    height: 100%;
 
-            &::before {
-                position: absolute;
-                content: '';
-                background: linear-gradient(235deg, #89ff00, #060c21, #00bcd4);
-                top: -2px;
-                bottom: -2px;
-                left: -2px;
-                right: -2px;
-                z-index: -1;
-            }
+    .neon_card {
+      width: 300px;
+      height: 400px;
+      background-image: url("../assets/fondo.jpg");
+      margin: 10px;
+      left: 30px;
+      position: relative;
+      @include displayCenter();
 
-            &::after {
-                position: absolute;
-                content: '';
-                background: linear-gradient(235deg, #89ff00, #060c21, #00bcd4);
-                top: -5px;
-                bottom: -5px;
-                left: -5px;
-                right: -5px;
-                z-index: -2;
-                filter: blur(50px);
-            }
-        }
+      &::before {
+        position: absolute;
+        content: '';
+        background: linear-gradient(235deg, #89ff00, #060c21, #00bcd4);
+        top: -2px;
+        bottom: -2px;
+        left: -2px;
+        right: -2px;
+        z-index: -1;
+      }
+
+      &::after {
+        position: absolute;
+        content: '';
+        background: linear-gradient(235deg, #89ff00, #060c21, #00bcd4);
+        top: -5px;
+        bottom: -5px;
+        left: -5px;
+        right: -5px;
+        z-index: -2;
+        filter: blur(50px);
+      }
     }
+  }
+
+@media (min-width: 962px) and (max-width: 1365px){
+
+  .container {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    width: 100%;
+    height: 100%;
+
+    .neon_card {
+      width: 300px;
+      height: 400px;
+      background-image: url("../assets/fondo.jpg");
+      margin: 10px;
+      left: 30px;
+      position: relative;
+      @include displayCenter();
+
+      &::before {
+        position: absolute;
+        content: '';
+        background: linear-gradient(235deg, #89ff00, #060c21, #00bcd4);
+        top: -2px;
+        bottom: -2px;
+        left: -2px;
+        right: -2px;
+        z-index: -1;
+      }
+
+      &::after {
+        position: absolute;
+        content: '';
+        background: linear-gradient(235deg, #89ff00, #060c21, #00bcd4);
+        top: -5px;
+        bottom: -5px;
+        left: -5px;
+        right: -5px;
+        z-index: -2;
+        filter: blur(50px);
+      }
+    }
+  }
+  .buttons{
+    margin-left: 80%;
+    margin-top: 6%;
+  }
+}
+@media (min-width: 361px) and (max-width: 960px){
+  .buttons{
+    right: 26%;
+    margin-top: 3%;
+  }
+
+  .container {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+    width: 100%;
+    height: 100%;
+
+    .neon_card {
+      width: 300px;
+      height: 400px;
+      background-image: url("../assets/fondo.jpg");
+      margin: 10px;
+      left: 185px;
+      position: relative;
+      justify-content: center;
+      align-items: center;
+
+      &::before {
+        position: absolute;
+        content: '';
+        background: linear-gradient(235deg, #89ff00, #060c21, #00bcd4);
+        top: -2px;
+        bottom: -2px;
+        left: -2px;
+        right: -2px;
+        z-index: -1;
+      }
+
+      &::after {
+        position: absolute;
+        content: '';
+        background: linear-gradient(235deg, #89ff00, #060c21, #00bcd4);
+        top: -5px;
+        bottom: -5px;
+        left: -5px;
+        right: -5px;
+        z-index: -2;
+        filter: blur(50px);
+      }
+    }
+  }
+}
 </style>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
